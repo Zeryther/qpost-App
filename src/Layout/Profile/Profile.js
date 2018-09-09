7d0d1816c7dd686c8713a8701403af20342a5b17
Zeryther <zeryther@gmail.com>
2018-09-09 23:58:48 +0200

    Display JSON on profile route

--- src/Layout/Profile/Profile.js
+++ src/Layout/Profile/Profile.js
@@ -1,18 +1,55 @@
 import React, { Component } from "react";
 import NavigationBar from "../NavigationBar";
 import { Container } from "reactstrap";
+import axios from 'axios';
+import SessionUtil from "../../Util/Session/SessionUtil";
 
 class Profile extends Component {
-	render() {
-		return (
-			<div className="home-feed">
-				<NavigationBar/>
+	constructor(props){
+		super(props);
 
-				<Container className="mt-3">
-					profile
-				</Container>
-			</div>
-		);
+		this.state = {
+			userMatch: this.props.match.params.query,
+			user: null
+		};
+
+		console.log(this.state.userMatch);
+	}
+
+	componentDidMount(){
+		axios.get("https://qpost.gigadrivegroup.com/api/user/info",{params: {token: SessionUtil.getSessionToken(), user: this.state.userMatch}})
+		.then(res => {
+			const user = res.data;
+
+			if(user && user.hasOwnProperty("id")){
+				SessionUtil.updateCurrentUser(user);
+				this.setState({user: user});
+			}
+		});
+	}
+
+	render() {
+		if(this.state.user !== null){
+			return (
+				<div className="profile">
+					<NavigationBar/>
+
+					{JSON.stringify(this.state.user)}
+				</div>
+			)
+		} else {
+			return (
+				<div className="profile">
+					<NavigationBar/>
+	
+					<Container className="mt-3">
+						<div className="text-center">
+							<i className="fas fa-spinner fa-pulse" style={{fontSize: "48px"}}></i>
+						</div>
+					</Container>
+				</div>
+			);
+		}
 	}
 }
 
