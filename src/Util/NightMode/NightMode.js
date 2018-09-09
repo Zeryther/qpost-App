9424d68246962ec8bac26af73a52fe89216e126f
Zeryther <zeryther@gmail.com>
2018-09-09 19:06:02 +0200

    Added NightMode utility class

---
+++ src/Util/NightMode/NightMode.js
@@ -0,0 +1,17 @@
+class NightMode {
+	static isActive(){
+		let storedVal = localStorage.getItem("nightMode");
+
+		return storedVal && storedVal === "true" ? true : false;
+	}
+
+	static toggle(){
+		this.setActive(!this.isActive());
+	}
+
+	static setActive(active){
+		localStorage.setItem("nightMode",active === true ? "true" : "false");
+	}
+}
+
+export default NightMode;
