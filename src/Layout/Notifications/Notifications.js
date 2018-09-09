60df377908c1fe98b75ff28d58470d39a53a7c32
Zeryther <zeryther@gmail.com>
2018-09-09 21:22:33 +0200

    Added dummy text to page components

--- src/Layout/Notifications/Notifications.js
+++ src/Layout/Notifications/Notifications.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import NavigationBar from "../NavigationBar";
+import { Container } from "reactstrap";
 
 class Notifications extends Component {
 	render() {
 		return (
 			<div className="home-feed">
 				<NavigationBar active="notifications"/>
+
+				<Container className="mt-3">
+					notifications
+				</Container>
 			</div>
 		);
 	}
--- src/Layout/Search/Search.js
+++ src/Layout/Search/Search.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import NavigationBar from "../NavigationBar";
+import { Container } from "reactstrap";
 
 class Search extends Component {
 	render() {
 		return (
 			<div className="home-feed">
 				<NavigationBar active="search"/>
+
+				<Container className="mt-3">
+					search
+				</Container>
 			</div>
 		);
 	}
