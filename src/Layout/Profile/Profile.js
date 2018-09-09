f18ac52f7b28260c44c95a4b8947c5dfaf6b9a8c
Zeryther <zeryther@gmail.com>
2018-09-10 00:49:14 +0200

    Update profile

--- src/Layout/Profile/Profile.js
+++ src/Layout/Profile/Profile.js
@@ -3,6 +3,7 @@ import NavigationBar from "../NavigationBar";
 import { Container } from "reactstrap";
 import axios from 'axios';
 import SessionUtil from "../../Util/Session/SessionUtil";
+import { Link } from "react-router-dom";
 
 class Profile extends Component {
 	constructor(props){
@@ -12,11 +13,21 @@ class Profile extends Component {
 			userMatch: this.props.match.params.query,
 			user: null
 		};
-
-		console.log(this.state.userMatch);
 	}
 
 	componentDidMount(){
+		this.fetch();
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot){
+		if(prevProps.match.params.query !== this.props.match.params.query){
+			this.setState({userMatch: prevProps.match.params.query,user: null});
+
+			this.fetch();
+		}
+	}
+
+	fetch = () => {
 		axios.get("https://qpost.gigadrivegroup.com/api/user/info",{params: {token: SessionUtil.getSessionToken(), user: this.state.userMatch}})
 		.then(res => {
 			const user = res.data;
@@ -33,6 +44,9 @@ class Profile extends Component {
 			return (
 				<div className="profile">
 					<NavigationBar/>
+
+					<Link to="/Zeryther">Zeryther</Link>
+					<Link to="/rex2go">rex2go</Link>
 
 					{JSON.stringify(this.state.user)}
 				</div>
